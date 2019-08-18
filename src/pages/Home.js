65a65937d107b0e469c7ebc36294ac562ff8369e
skin93 <arro.ciesielski@gmail.com>
2019-08-18 22:45:47 +0200

    hero component included

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Hero from '../components/Hero';
 
 const Home = () => {
-  return <div>Hello from Home page</div>;
+  return <Hero hero='defaultHero' />;
 };
 
 export default Home;
